d7e523ba88a341a8202698aa9d3a947c8b13da4e
Taber McFarlin <tabermcfarlin@gmail.com>
2019-07-14 23:11:30 -0400

    removed player accX

--- src/entity/actor/Player.hx
+++ src/entity/actor/Player.hx
@@ -13,11 +13,11 @@ class Player extends Actor {
 	private static inline var height:Int = 52;
 
 	// Movement parameters
-	private var moveSpeed:Float = 200;
+	private var moveSpeed:Float = 350;
 	private var accelTime:Float = 0.2;
-	private var deccelTime:Float = 0.01;
+	private var deccelTime:Float = 0.1;
 
-	private var jumpDist:Float = 200;
+	private var jumpDist:Float = 350;
 	private var jumpHeight:Float = 150;
 	private var jumpHeightMod:Float = 0.4;
 	private var airMobility:Float = 0.5;
@@ -73,16 +73,13 @@ class Player extends Actor {
 		// Friction
 		if (isGrounded() && velX != 0) {
 			if (sign(controller.xAxis) != sign(velX)) {
-				accX += -sign(velX) * friction;
 				velX = approach(velX, 0, friction * dt);
 			} else if (Math.abs(velX) > moveSpeed) {
-				accX += -sign(velX) * runReduce;
 				velX = approach(velX, sign(velX) * moveSpeed, runReduce * dt);
 			}
 		}
 
 		if (controller.xAxis != 0) {
-			accX += sign(controller.xAxis) * moveForce * mult;
 			velX = approach(velX, moveSpeed * controller.xAxis, moveForce * mult * dt);
 		}
 
@@ -100,8 +97,8 @@ class Player extends Actor {
 			if (lastRide != null && isRiding(lastRide)) {
 				velX += lastRide.velX;
 				velY += lastRide.velY;
-				lastRide = null;
 			}
+			lastRide = null;
 		}
 
 		return accY;
@@ -129,6 +126,15 @@ class Player extends Actor {
 
 	public override function isRiding(solid:Solid):Bool {
 		return col.intersectsAt(solid.col, x, y + 1);
+	}
+
+	public override function setRiding(solid:Solid) {
+		if (lastRide != solid) {
+			velX -= solid.velX;
+			velY -= solid.velY;
+		}
+
+		super.setRiding(solid);
 	}
 
 	public override function destroy() {
