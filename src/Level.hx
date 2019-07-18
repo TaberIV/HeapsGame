7c5fddf568d75e480f8dd172bd264596b962da7f
Taber <emcfarli@stevens.edu>
2019-07-17 23:00:32 -0400

    Data

--- src/Level.hx
+++ src/Level.hx
@@ -12,15 +12,6 @@ class Level extends h2d.Scene {
 
 		ents = new Array<Entity>();
 		col = new collision.CollisionSystem();
-
-		// ! Test
-		// ents.push(new entity.solid.Wall(this, 64, height - 182, 192, 32));
-		ents.push(new entity.solid.Wall(this, 3 * width / 4, height / 2, 32, 64));
-		ents.push(new entity.solid.Wall(this, 0, height - 32, width - 128, 32));
-		ents.push(new entity.solid.MovingWall(this, 3 * width / 4, height, 64, 32, 0, -50));
-		ents.push(new entity.solid.MovingWall(this, 0, height - 126, 64, 64, 64, 0));
-
-		ents.push(new entity.actor.Player(this, width / 2, height / 2));
 	}
 
 	public function addEntity(ent:Entity):Void {
