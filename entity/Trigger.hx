e03bb4488e98e0fff0ec765d898dfc9d6c911786
Taber <emcfarli@stevens.edu>
2019-08-02 01:06:01 -0400

    Triggers

--- entity/Trigger.hx
+++ entity/Trigger.hx
@@ -1,3 +1,30 @@
 package entity;
 
-class Trigger extends Entity {}
+import collision.Collider;
+import level.Level;
+import entity.Actor;
+
+/**
+	`Triggers` act when entered by an actor.
+**/
+class Trigger extends Entity {
+	public var onActorEnter:Actor->Void;
+
+	public function new(level:Level, x:Float, y:Float, width:Int, height:Int) {
+		super(level, x, y);
+
+		col = new Collider(this, width, height, 0, 0);
+	}
+
+	public static function levelTrigger(level:Level, x:Int, y:Int, w:Int, h:Int, tileSize:Int) {
+		return new Trigger(level, x * tileSize, y * tileSize, w * tileSize, h * tileSize);
+	}
+
+	public override function update(dt:Float) {
+		var actors = col.getOverlapingActors();
+
+		for (a in actors) {
+			onActorEnter(a);
+		}
+	}
+}
