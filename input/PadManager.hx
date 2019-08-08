ef43b6cd20912abfee9a0ebd5c0b69170c31f709
Taber <emcfarli@stevens.edu>
2019-08-08 16:26:33 -0400

    Manager connect/disconnect

--- input/PadManager.hx
+++ input/PadManager.hx
@@ -4,22 +4,28 @@ import hxd.Pad;
 
 class PadManager {
 	var pads:Map<Int, Pad>;
+	var users:Map<Int, Array<PadController>>;
 
 	public function new() {
 		pads = new Map();
+		users = new Map();
 
-		Pad.wait(onConnected);
+		Pad.wait(onConnect);
 	}
 
-	private function onConnected(pad:Pad) {
-		pads.set(pad.index, pad);
+	private function onConnect(pad:Pad) {
+		pad.onDisconnect = onDisconnect;
+		pads[pad.index] = pad;
+		users[pad.index] = new Array();
 
-		Pad.wait(onConnected);
+		Pad.wait(onConnect);
 	}
 
-	public function getPad(p:PadController, onPad:Pad->Void, ?index:Int) {
+	private function onDisconnect() {}
+
+	public function getPad(c:PadController, onPad:Pad->Void, ?index:Int) {
 		// Find an index with a controller
-		if (index == null) {
+		if (index == null || pads[index] == null || !pads[index].connected) {
 			for (i in pads.keys()) {
 				var pad = pads.get(i);
 
@@ -31,8 +37,9 @@ class PadManager {
 		}
 
 		// If no controller (with index if indicated, or at all otherwise) is found, wait.
-		if (index != null && pads[index] != null && pads[index].connected) {
+		if (index != null) {
 			onPad(pads[index]);
+			users[index].push(c);
 		}
 	}
 
